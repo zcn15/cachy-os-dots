dda82fd96534ff821ee4c5972d909ab1348253a0
zcn15 <mihkna2@gmail.com>
2026-05-17 21:43:17 +0200

    initial commit

---
+++ config/fish/config.fish
@@ -0,0 +1,13 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+alias ls='lsd'
+alias cat='bat'
+alias lsa='lsd -a'
+alias brtfetch='brrtfetch -width 50 -height 50 -fps 10 /home/daifu/Pictures/brrtfetch/gifs/ghastly.gif'
+alias config='cd .dots/config/'
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
---
+++ home/.bashrc
@@ -0,0 +1,12 @@
+#
+# ~/.bashrc
+#
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+PS1='[\u@\h \W]\$ '
+export PATH="$HOME/.local/bin:$PATH"
+export XDG_DATA_DIRS=$XDG_DATA_DIRS:/var/lib/flatpak/exports/share:/home/$(whoami)/.local/share/flatpak/exports/share
